4c0b8bd9d1774bda9c0bc584261add55d7cd903c
Awosika Ayomide <122920487+HonAysporto@users.noreply.github.com>
2026-06-21 01:55:26 +0100

    Update getorders.php

--- getorders.php
+++ getorders.php
@@ -1,6 +1,9 @@
 <?php
 
-require_once 'cors.php'; // MUST be first
+header("Access-Control-Allow-Origin: https://tazerh-store.vercel.app");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+header("Content-Type: application/json");
 
 // 🔥 Handle preflight request immediately
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
